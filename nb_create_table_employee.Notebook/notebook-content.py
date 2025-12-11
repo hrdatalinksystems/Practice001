1b757ba47408caedc26aa700e2786bc9ed3b66d7
hrdatalinksystems <hr@datalinksystems.co.uk>
2025-12-11 19:24:53 +0000

    Committing 1 item from workspace 71f0c899-a6f3-4cfb-8de8-a51b3f6e3e2a

---
+++ nb_create_table_employee.Notebook/notebook-content.py
@@ -0,0 +1,26 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC 
+# MAGIC create table employee (
+# MAGIC emp_id INT,
+# MAGIC emp_name VARCHAR(60)
+# MAGIC );
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
