96c9a9a37d3b726a2b8340e5093c486a9b3bc572
hrdatalinksystems <hr@datalinksystems.co.uk>
2025-12-11 19:49:20 +0000

    Committing 1 item from workspace 71f0c899-a6f3-4cfb-8de8-a51b3f6e3e2a

--- nb_util_get_fat.Notebook/notebook-content.py
+++ nb_util_get_fat.Notebook/notebook-content.py
@@ -16,8 +16,8 @@ import requests
 
 def get_access_token():
 
-    tenant_id = ""
-    client_id = ""
+    tenant_id = '4594a161-25b0-4798-a4d8-158b8df5b867'
+    client_id = 'd4da7d4f-58ab-4fae-9cf1-0d718fb99afb'
     client_secret = ""
 
     url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
