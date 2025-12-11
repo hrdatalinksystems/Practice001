2b7de25502091a4b5dae573b591f2fff5ba2d07c
hrdatalinksystems <hr@datalinksystems.co.uk>
2025-12-11 19:26:07 +0000

    Committing 1 item from workspace 71f0c899-a6f3-4cfb-8de8-a51b3f6e3e2a

---
+++ TestSQLdatabase.SQLDatabase/dbo/Tables/Employee.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[Employee] (
+    [EmpId]    INT           NOT NULL,
+    [EmpName]  NVARCHAR (50) NOT NULL,
+    [DeptName] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([EmpId] ASC)
+);
+
+
+GO
+
---
+++ TestSQLdatabase.SQLDatabase/dbo/Tables/customer.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[customer] (
+    [Customer Id] NVARCHAR (MAX) NULL,
+    [Age]         NVARCHAR (MAX) NULL,
+    [City Id]     NVARCHAR (MAX) NULL,
+    [City]        NVARCHAR (MAX) NULL,
+    [State]       NVARCHAR (MAX) NULL,
+    [Name]        NVARCHAR (MAX) NULL
+);
+
+
+GO
+
