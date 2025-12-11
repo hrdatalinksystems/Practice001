11fa51391ff562b0e872fb77e7b449c40656aedd
hrdatalinksystems <hr@datalinksystems.co.uk>
2025-12-11 19:48:34 +0000

    Committing 1 item from workspace 71f0c899-a6f3-4cfb-8de8-a51b3f6e3e2a

--- nb_util_get_fat.Notebook/notebook-content.py
+++ nb_util_get_fat.Notebook/notebook-content.py
@@ -13,23 +13,29 @@
 
 import requests
 
-def get_at():
 
-    ti = ""
-    ci = ""
-    cs = ""
+def get_access_token():
 
-    url = ""
+    tenant_id = ""
+    client_id = ""
+    client_secret = ""
+
+    url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
     headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
     data = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'scope': 'https://analysis.windows.net/powerbi/api/.default'
     }
 
     response = requests.post(url, headers=headers, data=data)
     response_json = response.json()
-     
-    return None
-
+    access_token = response_json.get('access_token')
+    
+    return access_token
 
 
 # METADATA ********************
